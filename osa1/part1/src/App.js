1851e93e87e42b360232f85a56c59878b99d3671
Kalle Ahlström <hashtagkalski@gmail.com>
2021-12-30 17:20:12 +0200

    exercises until 1.5

---
+++ osa1/part1/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const nimi = 'Pekka'
+  const ika = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={10} />
+      <Hello name={nimi} age={ika} />
+    </>
+  )
+}
+
+export default App
