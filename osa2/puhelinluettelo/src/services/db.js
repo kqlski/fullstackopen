44075bef1bd3f06057d77b2c02929a037ef155b9
Ahlström Kalle <hashtagkalski@gmail.com>
2022-01-03 20:04:29 +0200

    puhelinluettelon frontend muokkaukset

--- osa2/puhelinluettelo/src/services/db.js
+++ osa2/puhelinluettelo/src/services/db.js
@@ -1,6 +1,5 @@
-import React from "react";
 import axios from "axios";
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
