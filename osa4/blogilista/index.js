8ef4f078d56c79322fead2da183f8b0c768acfc4
Ahlström Kalle <hashtagkalski@gmail.com>
2022-01-04 15:28:54 +0200

    added router to handle HTTP-requests

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,33 +1,18 @@
 const config = require('./utils/config')
 const http = require('http')
 const express = require('express')
+const blogsRouter = require('./controllers/blogs')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const Blog = require('./models/blog')
 
 mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = config.PORT
 app.listen(PORT, () => {
