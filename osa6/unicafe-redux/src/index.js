9c9177cc9363fc6de16d0297216a1b29c159b868
Ahlström Kalle <hashtagkalski@gmail.com>
2022-01-10 20:08:36 +0200

    teht. 6.1-6.2

---
+++ osa6/unicafe-redux/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const action = (type) => {
+    store.dispatch({
+      type: type
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={() => action('GOOD')}>good</button>
+      <button onClick={()=>action('OK')}>ok</button>
+      <button onClick={()=>action('BAD')}>bad</button>
+      <button onClick={()=>action('ZERO')}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
