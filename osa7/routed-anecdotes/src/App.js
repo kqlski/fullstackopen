4a45631a052c5a816e3d0271cc9bf8f4a4a6c678
Ahlström Kalle <hashtagkalski@gmail.com>
2022-01-12 01:01:55 +0200

    teht. 7.4.-7.8.

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import {
   Link, Switch, Route, useRouteMatch
-  ,useHistory
+  , useHistory
 } from 'react-router-dom'
+import { useField } from './hooks'
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -57,38 +58,45 @@ const Footer = () => (
   </div>
 )
 
+
+
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
   }
-
+  const handleReset = (e) => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content'{...content} reset=''/>
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author'{...author} reset='' />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input name='info' {...info} reset=''/>
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='button' onClick={handleReset}>reset</button>
       </form>
     </div>
   )
@@ -112,7 +120,7 @@ const App = () => {
       id: '2'
     }
   ])
-  
+
   const history = useHistory()
 
   const [notification, setNotification] = useState('')
@@ -122,7 +130,7 @@ const App = () => {
     setAnecdotes(anecdotes.concat(anecdote))
     history.push('/')
     setNotification(`a new anecdote ${anecdote.content} created!`)
-    setTimeout(()=>setNotification(''),10000)
+    setTimeout(() => setNotification(''), 10000)
   }
 
   const anecdoteById = (id) =>
